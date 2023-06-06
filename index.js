84bd0457d963c10f4f029f049333b0839e9e3000
intecfrontend <53944321+intecfrontend@users.noreply.github.com>
2023-06-06 14:39:21 +0200

    r

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const url = "mongodb+srv://me:pass@cluster0.zwawlch.mongodb.net/?retryWrites=true&w=majority"
 const app = express()
+
+
+const url = "mongodb+srv://me:pass@cluster0.zwawlch.mongodb.net/?retryWrites=true&w=majority"
+async function connect(){
+  try{await mongoose.connect(url)
+  console.log("connected")}
+  catch(error){console.log(error)}
+}
 mongoose.connect(url)
+
 app.listen("4011", () => { console.log('listening on 4000') })
